febedf377f83705a59f340a41be2e9ebd0a778f3
sumi727 <80045226+sumi727@users.noreply.github.com>
2021-09-29 22:23:43 +0900

    Initial Commit

---
+++ controll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  controll
+//
+//  Created by 角友汰 on 2021/09/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
